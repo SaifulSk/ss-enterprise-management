b6d6b64d0c508201face972bab96895791c7faad
SaifulSk <ssaifrocks@gmail.com>
2026-07-10 01:49:57 +0530

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ss-enterprise-management/sw.js', { scope: '/ss-enterprise-management/' })})}
